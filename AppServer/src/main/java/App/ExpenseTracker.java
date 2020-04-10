f77b509a2b79dcbe166f300b7f3881665623f520
Siff <siff.ravn@gmail.com>
2020-04-10 11:59:28 +0200

    feat (AppServer) Implement App and add spring boot decorators to Boot MVC classes

--- AppServer/src/main/java/App/ExpenseTracker.java
+++ AppServer/src/main/java/App/ExpenseTracker.java
@@ -1,4 +1,13 @@
 package App;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ExpenseTracker {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTracker.class, args);
+    }
+
 }
