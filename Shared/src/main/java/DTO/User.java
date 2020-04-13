3e0184330bb82647fad951a0647f54d5fd3d205a
Siff <siff.ravn@gmail.com>
2020-04-13 17:07:12 +0200

    feat (DTO) Implement User

--- Shared/src/main/java/DTO/User.java
+++ Shared/src/main/java/DTO/User.java
@@ -1,4 +1,20 @@
 package DTO;
 
+import brugerautorisation.data.Bruger;
+
 public class User {
+
+    public String username; // studentID
+    public String firstName;
+    public String lastName;
+    public String study;
+    public String email;
+
+    public User(Bruger bruger){
+        username = bruger.brugernavn;
+        firstName = bruger.fornavn;
+        lastName = bruger.efternavn;
+        study = bruger.studeretning;
+        email = bruger.email;
+    }
 }
