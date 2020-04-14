22da5ea3ca24eb8acde805e4475fceec284d0246
Siff <siff.ravn@gmail.com>
2020-04-14 21:37:18 +0200

    Fix (APP) Repackage RESTControllers so SpringApp recognise them

--- AppServer/src/main/java/App/DBController.java
+++ AppServer/src/main/java/App/DBController.java
@@ -1,7 +1,7 @@
-package Controllers;
+package App;
 
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class DBController {
 }
--- AppServer/src/main/java/App/UserController.java
+++ AppServer/src/main/java/App/UserController.java
@@ -1,7 +1,7 @@
-package Controllers;
+package App;
 
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DBController {
+public class UserController {
 }
