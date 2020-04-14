f864382182d93846c608f9302616308f963bb1af
Siff <siff.ravn@gmail.com>
2020-04-14 21:45:03 +0200

    Feat (UserController) Implement hard code login()

--- AppServer/src/main/java/App/UserController.java
+++ AppServer/src/main/java/App/UserController.java
@@ -1,7 +1,31 @@
 package App;
 
-import org.springframework.web.bind.annotation.RestController;
+import DTO.User;
+import brugerautorisation.data.Bruger;
+import brugerautorisation.transport.rmi.Brugeradmin;
+import org.springframework.web.bind.annotation.*;
 
+import java.rmi.Naming;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 public class UserController {
+
+    @RequestMapping("/uc-test")
+    public String start() throws Exception {
+        return "Connected";
+    }
+
+    @GetMapping("/login")
+    public User login() throws Exception {
+        Brugeradmin admin = connectUserDB();
+
+        Bruger bruger = admin.hentBruger("s173998", "kk29");
+
+        return new User(bruger);
+    }
+
+    private Brugeradmin connectUserDB() throws Exception{
+        return (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
+    }
 }
