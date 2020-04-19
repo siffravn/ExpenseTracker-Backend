a08bb168ac704cd6efc503334ad36d99e3713717
Helle <helle1926@hotmail.com>
2020-04-19 15:46:17 +0200

    Attributes, get and setters to budget

--- Shared/src/main/java/DTO/Budget.java
+++ Shared/src/main/java/DTO/Budget.java
@@ -1,4 +1,29 @@
 package DTO;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Budget {
+
+    public ArrayList<Integer> budget;
+    public Date date;
+
+
+    public void setBudget(ArrayList<Integer> budgetList) {
+        this.budget = budgetList;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public ArrayList<Integer> getBudget() {
+        return budget;
+    }
+
+    public Date getDate() {
+        return date;
+    }
 }
+
+
