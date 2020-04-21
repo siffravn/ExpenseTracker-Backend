4754a299b0e8adecaec7f4ef12c34b074741a2c8
Siff <siff.ravn@gmail.com>
2020-04-21 14:40:05 +0200

    Feat (MockedData): Create Budget Data

--- AppServer/src/main/java/MockupData/MockedBudgetData.java
+++ AppServer/src/main/java/MockupData/MockedBudgetData.java
@@ -1,15 +1,17 @@
 package MockupData;
 
 import DTO.Budget;
+import DTO.BudgetPost;
 
-import java.security.PublicKey;
+import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class MockedBudgetData {
     // Array of Budget and Date in Budget
-    private ArrayList<Budget> budget;
+    private ArrayList<Budget> budgets = new ArrayList<Budget>();
     private static MockedBudgetData instance = null;
+
     public static MockedBudgetData getInstance(){
         if (instance== null){
             instance = new MockedBudgetData();
@@ -19,13 +21,32 @@ public class MockedBudgetData {
 
     //mocking the data
     public MockedBudgetData(){
-        budget = new ArrayList<Budget>();
-        budget.add(new Budget(300, new Date()));
+        Budget b = new Budget(YearMonth.of(2020, 4));
+        b.addPost(new BudgetPost("Groceries", 1500));
+        b.addPost(new BudgetPost("Bills", 5000));
+        b.addPost(new BudgetPost("Entertainment", 500));
+        budgets.add(b);
+
+        Budget c = new Budget(YearMonth.of(2020, 5));
+        c.addPost(new BudgetPost("Groceries", 1500));
+        c.addPost(new BudgetPost("Bills", 200));
+        c.addPost(new BudgetPost("Vacation", 5000));
+        budgets.add(c);
     }
 
     public ArrayList<Budget> getBudgetList(){
-        return budget;
+        return budgets;
+    }
 
+    public Budget getBudget(YearMonth yearMonth){
+        for (Budget budget : budgets){
+            if (budget.getDate().equals(yearMonth)) return budget;
+        }
+        return null;
+    }
+
+    public void createBudget(Budget budget){
+        budgets.add(budget);
     }
 
 }
