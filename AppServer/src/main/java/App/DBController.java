706ba26fd0851639cf8a6f45cc533e209c569760
Anekel <43749071+Anekel@users.noreply.github.com>
2020-04-21 11:56:14 +0200

    added crossorigin to DBController

--- AppServer/src/main/java/App/DBController.java
+++ AppServer/src/main/java/App/DBController.java
@@ -2,10 +2,13 @@ package App;
 
 import DTO.Expense;
 import MockupData.MockedExpenseData;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
 
 @RestController
 public class DBController {
