cc085ca45e8d2a97efcb26e98796c717e0177c31
magnusbrok <magnus.brok@db-data.dk>
2020-04-24 14:38:46 +0200

    changed crossorigin

--- AppServer/src/main/java/App/DBController.java
+++ AppServer/src/main/java/App/DBController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-@CrossOrigin(origins = {"https://expense-tracker-dfe53.web.app/"})
-
+//@CrossOrigin(origins = {"https://expense-tracker-dfe53.web.app/"})
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 public class DBController {
     MockedBudgetData mockedBudgetData = MockedBudgetData.getInstance();
