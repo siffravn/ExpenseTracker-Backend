7155d6b282120c0a870ec2c0e6214e8dcf0ca020
Helle <helle1926@hotmail.com>
2020-04-28 17:46:37 +0200

    To be continued - still not working

--- ConsoleClient/src/main/java/Console/BudgetRESTConsoleClient.java
+++ ConsoleClient/src/main/java/Console/BudgetRESTConsoleClient.java
@@ -1,11 +1,10 @@
 package Console;
 import DTO.User;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MultivaluedHashMap;
-import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Scanner;
 
@@ -17,7 +16,6 @@ public class BudgetRESTConsoleClient {
     public static void main(String[] args) {
         Client client = ClientBuilder.newClient();
         login(client);
-
     }
 
     public static void login(Client client) {
@@ -32,8 +30,11 @@ public class BudgetRESTConsoleClient {
         String url = domain + "/login";
         LogIn logIn = new LogIn(username,password);
 
-        Response response = client.target(url).request().post(Entity.json(logIn), Response.class);
-        User result = response.readEntity(User.class);
+        //Response response = client.target(url).request().post(Entity(logIn), Response.class);
+        Response response = client.target(url).request().get();
+        clientID = username;
+
+        String result = response.readEntity(String.class);
 
         System.out.println(result);
     }
