c88062469d607ce15f8811dbd92019e2cc48ebff
Siff <siff.ravn@gmail.com>
2020-05-01 14:14:33 +0200

    Feat (ConsoleClient) Simplify login backendService method

--- ConsoleClient/src/main/java/Console/BudgetRESTConsoleClient.java
+++ ConsoleClient/src/main/java/Console/BudgetRESTConsoleClient.java
@@ -12,36 +12,19 @@ import java.util.Scanner;
 
 public class BudgetRESTConsoleClient {
 
-    static String domain ="http://localhost:3344"; // TODO change when server is deployed
-    static User user;
+    private String domain ="http://localhost:3344"; // TODO change when server is deployed
+    private Client client = ClientBuilder.newClient();
 
-    public static void main(String[] args) {
-        Client client = ClientBuilder.newClient();
-        login(client);
-    }
-
-    public static void login(Client client) {
-        System.out.println("Welcome to ExpenseTracker!");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your login information:");
-        System.out.println("Username: ");
-        String username = scanner.next();
-        System.out.println("Password: ");
-        String password = scanner.next();
+    public User login(String username, String password) {
 
         LogIn logIn = new LogIn(username,password);
 
-        WebTarget webTarget = client.target(domain);
-
-        WebTarget authTarget = webTarget.path("/login");
-
-        Invocation.Builder invocationBuilder = authTarget.request(MediaType.APPLICATION_JSON);
-
-        Response response = invocationBuilder.post(Entity.entity(logIn, MediaType.APPLICATION_JSON), Response.class);
-
-        User user = response.readEntity(User.class);
-
-        System.out.println("Your username is: " + user.username);
+        return client
+                .target(domain)
+                .path("/login")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(logIn, MediaType.APPLICATION_JSON))
+                .readEntity(User.class);
      }
 
      public Budget getBudget(int year, int month){
