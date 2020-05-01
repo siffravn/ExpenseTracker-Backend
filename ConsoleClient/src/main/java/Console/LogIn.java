4bb79e8358c22ce2b4b4e2c86c3dcfd051d4e2d6
Siff <siff.ravn@gmail.com>
2020-05-01 12:26:34 +0200

    Feat (LogIn) Implement Serializable

--- ConsoleClient/src/main/java/Console/LogIn.java
+++ ConsoleClient/src/main/java/Console/LogIn.java
@@ -1,11 +1,31 @@
 package Console;
 
-public class LogIn {
+import java.io.Serializable;
+
+public class LogIn implements Serializable {
     private String username ="s180317";
     private String password ="180317";
 
+    public LogIn() {}
+
     public LogIn(String username, String password){
         this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
